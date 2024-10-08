c7b70b5fb18e88f121fab0febcbe98fa1d467c23
lijiangtao <3530812934@qq.com>
2024-10-08 21:59:07 +0800

    完善请求拦截器的错误提示

--- src/utils/request.js
+++ src/utils/request.js
@@ -11,6 +11,7 @@ request.interceptors.request.use(
     return config
   },
   (err) => {
+    console.log(err)
     return Promise.reject(err)
   }
 )
@@ -18,13 +19,15 @@ request.interceptors.request.use(
 request.interceptors.response.use(
   (response) => {
     // 错误响应处理
+    console.log(response)
     if (response.status < 200 || response.status >= 300) {
-      ElMessage.error(response.data.message || '业务异常')
+      ElMessage.error(response.data.error_description || '业务异常')
       return Promise.reject(response.data)
     }
     return response.data
   },
   (err) => {
+    ElMessage.error(err.response.data.error_description || '业务异常')
     return Promise.reject(err)
   }
 )
